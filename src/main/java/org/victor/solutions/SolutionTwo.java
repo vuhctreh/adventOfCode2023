b9a63c2ea36058a38a355f798240d41c34108304
Victor <victor.lecardonnel@hotmail.fr>
2023-12-02 16:50:29 +0000

    feat: solved day 2 part 2

--- src/main/java/org/victor/solutions/SolutionTwo.java
+++ src/main/java/org/victor/solutions/SolutionTwo.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class SolutionTwo {
     public static void solve(List<String> input) {
         int sum = 0;
+        int setSum = 0;
 
         Map<String, Integer> map = new HashMap<>(
                 Map.of(
@@ -23,11 +24,24 @@ public class SolutionTwo {
 
             int possible = 1;
 
+            Map<String, Integer> minSet = new HashMap<>(
+                    Map.of(
+                            "red", 0,
+                            "green", 0,
+                            "blue", 0
+                    )
+            );
+
             for (List<List<String>> game : games) {
                 for(List<String> roll : game) {
-                    if(map.get(roll.get(1)) < Integer.parseInt(roll.get(0))) {
+                    int rollNo = Integer.parseInt(roll.get(0));
+
+                    int min = Math.max(minSet.get(roll.get(1)), rollNo);
+
+                    minSet.put(roll.get(1), min);
+
+                    if(map.get(roll.get(1)) < rollNo) {
                         possible = 0;
-                        break;
                     }
                 }
             }
@@ -35,8 +49,11 @@ public class SolutionTwo {
             if(possible == 1) {
                 sum += i + 1;
             }
+
+            setSum += minSet.get("red") * minSet.get("green") * minSet.get("blue");
         }
-        System.out.println(sum);
+        System.out.println("Sum of possible games: " + sum);
+        System.out.println("Sum cube set power: " + setSum);
     }
 
     public static List<List<List<String>>> parsed(String s) {
@@ -53,11 +70,11 @@ public class SolutionTwo {
             List<List<String>> list = new ArrayList<>();
 
             // For each string in split array
-            for(int j = 0; j < split.length; j++) {
+            for (String string : split) {
                 // For each character in string
-                for(int k = 0; k < split[j].length(); k++) {
-                    if(!Character.isDigit(split[j].charAt(k))) {
-                        list.add(List.of(split[j].substring(0, k), split[j].substring(k)));
+                for (int k = 0; k < string.length(); k++) {
+                    if (!Character.isDigit(string.charAt(k))) {
+                        list.add(List.of(string.substring(0, k), string.substring(k)));
                         break;
                     }
                 }
